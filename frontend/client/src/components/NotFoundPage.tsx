228b1d584288d2126dd7cf3a3b4886ff2ab0833e
paulo.rmoreira <paulo.rmoreira@dataprev.gov.br>
2026-01-23 13:10:22 -0300

    zerou

--- frontend/client/src/components/NotFoundPage.tsx
+++
@@ -1,5 +0,0 @@
-
-import React from 'react';
-
-export default () => <h3>The requested page has not been found.</h3>;
-
--- frontend/server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function (req, res) {
-  res.json({ service: 'react-petclinic-server' });
-});
-
-module.exports = router;
